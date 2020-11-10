4337d1aeb57c25e0ddf9c01bd49cc4a8c0dd6a24
Dave Kitchen <51515342+davekitchen@users.noreply.github.com>
2020-11-10 11:47:57 +0000

    Update Program.cs

--- SQLPaaSConnectionTest/Program.cs
+++ SQLPaaSConnectionTest/Program.cs
@@ -7,7 +7,7 @@ namespace SQLPaaSConnectionTest
     {
         static void Main(string[] args)
         {
-            string conStr = "Server=mkdcontest.database.windows.net,1433; Database=Contest; User ID=<UserName>;Password=<Password?;Trusted_Connection=False;Encrypt=True";
+            string conStr = "Server=mkdcontest.database.windows.net,1433; Database=Contest; User ID=<UserName>;Password=<Password>;Trusted_Connection=False;Encrypt=True";
 
             using (SqlConnection conMain = new SqlConnection(conStr))
             {
